e37ac373797871414024302d98cf9f8ff3029411
ruisucode <theboss7lol@gmail.com>
2023-10-29 14:38:36 -0400

    refactor:footer
    * pb:1

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -4,7 +4,7 @@ import Copyright from "./copyright";
 export default function Footer() {
   return (
     <>
-      <Box component="footer" sx={{ bgcolor: "transparent", pt: 6 }}>
+      <Box component="footer" sx={{ bgcolor: "transparent", pt: 6, pb: 1 }}>
         <Container maxWidth="lg">
           <Typography variant="h6" align="center" gutterBottom>
             Investigaciones
